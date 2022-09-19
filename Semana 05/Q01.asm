37a2b2ac241db60f97061434de6b67923b24a40b
Bruno Rafael dos Santos <62629549+bruniculos08@users.noreply.github.com>
2022-09-19 09:22:34 -0300

    update

--- Semana 05/Q01.asm
+++ Semana 05/Q01.asm
@@ -1,19 +1,36 @@
 # Contador de dígitos
 .data
+    jumpLine: .ascii "\n"
 .text
 .globl main
 
 main:
+
+    while:
     li $v0, 5
     syscall
-    move $a0, $v0
+    move $a0, $v0 # $a0 = n
+
+    beq $a0, $zero, end
+
+    li $v0, 5      
+    syscall
+    move $a1, $v0 # $a1 = k
+
     jal digitCount
 
-    # Para contar quantos dígitos iguais a um determinado número, basta dividir por 10 e verificar o resto
+    # Para contar quantos dígitos iguais a um determinado número k, basta dividir por 10 e verificar o resto e o quociente,...
+    # ... ai se o resto for igual k, some 1 + quantidade de k no quosciente.
 
     move $a0, $v0
     li $v0, 1
     syscall
+
+    li $v0, 4
+    la $a0, jumpLine
+    syscall
+
+    j while
 
 end:
     li $v0, 10
@@ -21,20 +38,29 @@ end:
 
 digitCount:
     # Salvar variáveis na pilha:
-    addi $sp, $sp, -4
+    addi $sp, $sp, -16
     sw $ra, 0($sp)
+    sw $a0, 4($sp)
+    sw $a1, 8($sp)
+    sw $s0, 12($sp)
 
     # Função:
     li $v0, 0
-    beq $a0, $zero, endDigitCount
     li $t0, 10
-    div $a0, $t0    
-    mflo $a0         #(carrega o resultado da divisão para $a0)
+    div $a0, $t0 
+    mflo $a0    # $a0 = 0
+    mfhi $s0    # $s0 = 1
+    beq $a0, $zero, verifyRest # (1) Se o quociente da divisão for 0 não continua a recursão
     jal digitCount
-    add $v0, $v0, 1
+    verifyRest:
+    bne $s0, $a1, endDigitCount # (2) Se o resto da divisão for igual à k, soma 1 no contador (retorno)
+    addi $v0, $v0, 1
     
     # Restaurar variáveis:
     endDigitCount:
+    lw $s0, 12($sp)
+    lw $a1, 8($sp)
+    lw $a0, 4($sp)
     lw $ra, 0($sp)
-    addi $sp, $sp, 4
+    addi $sp, $sp, 16
     jr $ra
